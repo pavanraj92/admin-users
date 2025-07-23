63d3170bf0b89bc42a54afc14972b61503fa0030
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-07-23 18:54:37 +0530

    changes in mail template

--- src/Mail/WelcomeMail.php
+++ src/Mail/WelcomeMail.php
@@ -3,7 +3,10 @@
 namespace admin\users\Mail;
 
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class WelcomeMail extends Mailable
@@ -21,13 +24,22 @@ class WelcomeMail extends Mailable
 
     public function build()
     {
-        return $this->subject('Welcome to ' . env('APP_NAME'))
+        $emailTemplate = \DB::table('emails')->where('slug', 'register_user')->first(['subject', 'description']);
+
+        $subject = $emailTemplate->subject;
+        $content = $emailTemplate->description;
+
+        $content = str_replace('%EMAIL_FOOTER%', config('GET.email_footer_text'), $content);
+        $subject = str_replace('%APP_NAME%', env('APP_NAME'), $subject);
+        $content = str_replace('%APP_NAME%', env('APP_NAME'), $content);
+        $content = str_replace('%USER_NAME%', $this?->user?->full_name, $content);
+
+        $content = str_replace('%EMAIL_ADDRESS%', $this?->user?->email, $content);
+        $content = str_replace('%PASSWORD%', $this->plainPassword, $content);
+        $result = $this->subject($subject)
             ->from(env('MAIL_FROM_ADDRESS'), env('MAIL_FROM_NAME'))
+            ->replyTo(env('MAIL_FROM_ADDRESS'))
             ->view('user::admin.email.welcome_mail')
-            ->with([
-                'fullName' => $this->user->full_name,
-                'email' => $this->user->email,
-                'password' => $this->plainPassword,
-            ]);
+            ->with(['template' => $content]);
     }
 }
